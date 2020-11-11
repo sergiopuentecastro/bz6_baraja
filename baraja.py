f31942440836f603b54ce6ebce598d631dcd78f2
Sergio Puente Castro <sergiopuentecastro@MacBook-Air-de-Sergio.local>
2020-11-11 23:05:31 +0100

    Funcion Barajar

--- baraja.py
+++ baraja.py
@@ -9,3 +9,17 @@ def creaBaraja():
     return baraja
 
 
+def intercambio (primer_valor, segundo_valor):
+    aux = primer_valor
+    primer_valor = segundo_valor
+    segundo_valor = aux
+    return primer_valor, segundo_valor
+
+
+def barajar(lista_de_naipes):
+    for i in range(len(lista_de_naipes)):
+        nueva_pos = random.randrange(len(lista_de_naipes))
+        aux = lista_de_naipes[nueva_pos]
+        lista_de_naipes[nueva_pos] = lista_de_naipes[i]
+        lista_de_naipes[i] = aux
+    return lista_de_naipes
