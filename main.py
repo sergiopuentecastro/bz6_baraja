453832674827af19e626581117b90f79b0fb9839
Sergio Puente Castro <sergiopuentecastro@MacBook-Air-de-Sergio.local>
2020-11-12 22:21:37 +0100

    Metodo Reparit en clase Baraja

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
 
 miBaraja = barajaC.Baraja(palos, numeros)
 
+miBaraja.mazacote[2]
+
 print(miBaraja.mazacote)
 
 print(miBaraja.repartir(3, 5))
